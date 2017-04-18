373e8aa62bc49cea24df0d6a2285b14a1f327dda
ghonix <ghonix@gmail.com>
2017-04-18 14:29:33 -0700

    Initial Commit

---
+++ src/main/java/me/ghonix/practice/TwoSum.java
@@ -0,0 +1,33 @@
+package me.ghonix.practice;
+
+import java.util.*;
+
+/**
+ * Created by aghoneim on 4/13/17.
+ */
+public class TwoSum {
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> numberSet = new HashMap<>();
+        for (int i = 0; i < nums.length; i ++) {
+            Integer value = numberSet.get(target - nums[i]);
+            if ( value != null) {
+                return new int[]{ value, i };
+            } else {
+                numberSet.put(nums[i], i);
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] result = twoSum(new int[] {2, 0, 7, 11, 15}, 15);
+
+        if (result != null) {
+            System.out.println(String.format("%d, %d", result[0], result[1]));
+        } else {
+            System.out.println("Result Not Found!");
+        }
+    }
+}
