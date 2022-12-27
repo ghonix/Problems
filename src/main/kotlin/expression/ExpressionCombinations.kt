4a3e0e2c6761eb04989f7d6a87e451f6139eac3c
Amr Ghoneim <ghonix@gmail.com>
2022-12-27 06:11:55 -0800

    Solve how different ways to evaluate an expression

---
+++ src/main/kotlin/expression/ExpressionCombinations.kt
@@ -0,0 +1,56 @@
+package expression
+
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.math.exp
+
+class ExpressionCombinations {
+    companion object {
+        @JvmStatic
+        fun main(args: Array<String>) {
+
+            println(ExpressionCombinations().diffWaysToCompute("2-2-1").toString())
+        }
+    }
+
+    fun diffWaysToCompute(expression: String): List<Int> {
+        val memory = HashMap<String, LinkedList<Int>>()
+        return diffWaysToCompute(expression, memory)
+    }
+
+
+    fun diffWaysToCompute(expression: String, memory: HashMap<String, LinkedList<Int>>): List<Int> {
+        var results: LinkedList<Int> = memory[expression] ?: LinkedList()
+
+        if (results.isNotEmpty()) {
+            return results
+        }
+
+        for (i in expression.indices) {
+            if (expression[i] == '+' ||
+                expression[i] == '-' ||
+                expression[i] == '*'
+            ) {
+                val leftWays = diffWaysToCompute(expression.substring(0, i), memory)
+                val rightWays = diffWaysToCompute(expression.substring(i + 1), memory)
+                for (left in leftWays) {
+                    for (right in rightWays) {
+                        results.add(
+                            when (expression[i]) {
+                                '+' -> left + right
+                                '-' -> left - right
+                                '*' -> left * right
+                                else -> 0
+                            }
+                        )
+                    }
+                }
+            }
+        }
+        if (results.isEmpty()) {
+            results.add(expression.toInt())
+        }
+
+        return results
+    }
+}
